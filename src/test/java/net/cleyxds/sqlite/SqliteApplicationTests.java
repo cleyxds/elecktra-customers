2260e6b886e36bfe17d093d05ed9b7bd8cc051a2
cleyxds <cleysonn@hotmail.com.br>
2021-04-07 22:30:27 -0300

    Initial files

---
+++ src/test/java/net/cleyxds/sqlite/SqliteApplicationTests.java
@@ -0,0 +1,13 @@
+package net.cleyxds.sqlite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqliteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
